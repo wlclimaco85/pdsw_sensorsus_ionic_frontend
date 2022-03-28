1655680c112020384489aaf18c4f164b1c21e98f
unknown <wlclimaco@hotmail.com>
2022-03-28 19:00:41 -0300

    test

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -1,6 +1,5 @@
 import { IonicPageModule } from 'ionic-angular/module';
 import { NgModule } from '@angular/core';
-import { Chart } from 'chart.js';
 import { HomePage } from './home';
 @NgModule({
     declarations: [HomePage],
